fb32a635c6eb02cf461351f780a452f9505fef4a
Kelvin zawala <87924723+Zawala@users.noreply.github.com>
2026-04-04 16:00:08 +0200

    register    the KafkaTemplate bean automatically

--- src/test/java/capital/one/capital/CapitalApplicationTests.java
+++ src/test/java/capital/one/capital/CapitalApplicationTests.java
@@ -2,13 +2,13 @@ package capital.one.capital;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class CapitalApplicationTests {
 
-	@MockBean
+	@MockitoBean
 	KafkaTemplate<String, String> kafkaTemplate;
 
 	@Test
